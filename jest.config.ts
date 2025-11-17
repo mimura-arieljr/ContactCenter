c67616af80e0b1c234351c4b840db1bc35b581a0
chockmimura <avmimurajr@gmail.com>
2025-11-17 22:29:19 +0800

    CC: Added jest config

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
+  },
+};
